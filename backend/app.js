3af6e24d9e150fc1a7c36a042a0cc83a82a95f8d
cedrick lam <cedricklam237@gmail.com>
2026-05-18 17:10:33 +0100

    Connexion à MongoDB échouée

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,50 @@
 const express = require('express');
 
+const mongoose = require('mongoose');
+
 const app = express();
 
+mongoose.connect('mongodb://cedrick:lamar237@ac-iuzi2cm-shard-00-00.1u02cwq.mongodb.net:27017,ac-iuzi2cm-shard-00-01.1u02cwq.mongodb.net:27017,ac-iuzi2cm-shard-00-02.1u02cwq.mongodb.net:27017/?ssl=true&replicaSet=atlas-k5rzli-shard-0&authSource=admin&appName=Cluster0',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.post('/api/stuff', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'Objet créé !'
+  });
+});
+
+app.get('/api/stuff', (req, res, next) => {
+  const stuff = [
+    {
+      _id: 'oeihfzeoi',
+      title: 'Mon premier objet',
+      description: 'Les infos de mon premier objet',
+      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      price: 4900,
+      userId: 'qsomihvqios',
+    },
+    {
+      _id: 'oeihfzeoi',
+      title: 'Mon deuxieme objet',
+      description: 'Les infos de mon premier objet',
+      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      price: 2900,
+      userId: 'qsomihvqios',
+    },
+  ];
+  res.status(200).json(stuff);
+});
 module.exports = app;
